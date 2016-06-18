68d9efdd3099e5d23c388f8f7aec842644c661eb
Charles Holbrow <charlesholbrow@gmail.com>
2016-06-18 16:15:24 -0400

    tileset cleaning, linter fixes

--- packages/tileset-elements/tileset-elements.js
+++ packages/tileset-elements/tileset-elements.js
@@ -2,33 +2,33 @@ import { TilesetDDS } from  'meteor/game-server-tilesets';
 
 // Static Tilesets
 TilesetDDS.add({
-  "name":"elements",
-  "imageUrl": "/packages/tileset-elements/img/elements9x3.png",
-  "width": 9,
-  "height": 3,
-  "tileWidth": 28,
-  "tileHeight": 35,
-  "cellWidth": 30,
-  "cellHeight": 37,
+  name: 'elements',
+  imageUrl: '/packages/tileset-elements/img/elements9x3.png',
+  width: 9,
+  height: 3,
+  tileWidth: 28,
+  tileHeight: 35,
+  cellWidth: 30,
+  cellHeight: 37,
   tileProperties: {
     opaque: [
       0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 1, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+      0, 0, 0, 0, 0, 0, 0, 0, 0],
     obstructed: [
       1, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 1, 0, 0, 0, 0, 0, 1, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+      0, 0, 0, 0, 0, 0, 0, 0, 0],
   },
   tileNames: {
-    tree:1,
-    grass:10,
-    water:11,
-    ice:12,
-    dirt:13,
-    lava:[14,15],
-    path:16,
-    stoneWall:17,
-    sand:18,
-  }
+    tree: 1,
+    grass: 10,
+    water: 11,
+    ice: 12,
+    dirt: 13,
+    lava: [14, 15],
+    path: 16,
+    'stone wall': 17,
+    sand: 18,
+  },
 });
