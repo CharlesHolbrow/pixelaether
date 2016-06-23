34f5ef2af3cf0c5580a906b4064d5726477a5f3c
Charles Holbrow <charlesholbrow@gmail.com>
2016-06-23 16:01:52 -0400

    light/light.js uses forEach iterators

--- packages/light/light.js
+++ packages/light/light.js
@@ -1,15 +1,3 @@
-// In this file we use some For In loops, becaue babel compiles
-// for-of loops to much more complicated code.
-//
-// New Discovery: The folllowing compiles nicely. Consider
-// switching to this syntax:
-//
-// Object.keys(this.catalog).forEach(() => {  });
-//
-// In the mean time disable for in loop errors in eslint.
-
-/* eslint no-restricted-syntax: ["off", "ForInStatement"] */
-
 import { Coord } from 'meteor/coord';
 
 export class LightCatalog {
@@ -21,10 +9,10 @@ export class LightCatalog {
 
   forEach(func) {
     Object.keys(this.catalog).forEach((cx) => {
-      const cxInt = parseInt(cx);
-      const cxObj = this.catalog(cx);
+      const cxInt = parseInt(cx, 10);
+      const cxObj = this.catalog[cx];
       Object.keys(cxObj).forEach((cy) => {
-        const cyInt = parseInt(cy);
+        const cyInt = parseInt(cy, 10);
         func(cxObj[cy], cxInt, cyInt);
       });
     });
@@ -33,22 +21,13 @@ export class LightCatalog {
   // According to stackoverflow, delete during iteration is ok:
   // http://stackoverflow.com/questions/3463048/is-it-safe-to-delete-an-object-property-while-iterating-over-them
   clean() {
-    for (const cx in this.catalog) {
-      if (this.catalog.hasOwnProperty(cx)) {
-        const cxObj = this.catalog[cx];
-        for (const cy in cxObj) {
-          if (cxObj.hasOwnProperty(cy)) {
-            if (Object.keys(cxObj[cy]).length === 0) {
-
-              delete cxObj[cy];
-            }
-          }
-        }
-        if (Object.keys(this.catalog[cx]).length === 0) {
-          delete this.catalog[cx];
-        }
-      }
-    }
+    Object.keys(this.catalog).forEach((cx) => {
+      const cxObj = this.catalog[cx];
+      Object.keys(cxObj).forEach((cy) => {
+        if (Object.keys(cxObj[cy]).length === 0) delete cxObj[cy];
+      });
+      if (Object.keys(this.catalog[cx]).length === 0) delete this.catalog[cx];
+    });
   }
 
   // maxRadius is an integer number of tiles
